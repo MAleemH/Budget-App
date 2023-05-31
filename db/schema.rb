a8f1c1bc9bd1f008ffb42ad84189322a49f81062
MAleemH <aleemhashmi4321@gmail.com>
2023-05-31 19:47:41 +0500

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_072552) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_142513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "group_payments", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "payment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_payments_on_group_id"
+    t.index ["payment_id"], name: "index_group_payments_on_payment_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_groups_on_author_id"
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_payments_on_author_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -22,12 +50,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_072552) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "group_payments", "groups"
+  add_foreign_key "group_payments", "payments"
+  add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "payments", "users", column: "author_id"
 end
