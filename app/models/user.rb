a0cb7ce36d37a4e564448358735ff3d61ad3d126
MAleemH <aleemhashmi4321@gmail.com>
2023-05-31 19:47:28 +0500

    Added relationships in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :groups, foreign_key: 'author_id'
+  has_many :payments, foreign_key: 'author_id'
+
   validates :name, presence: true
 end
