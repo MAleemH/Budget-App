d9369963d0c46e01fba7bad786223eaa7d470eb1
MAleemH <aleemhashmi4321@gmail.com>
2023-06-01 16:20:45 +0500

    Added splash screen

---
+++ app/controllers/splash_controller.rb
@@ -0,0 +1,5 @@
+class SplashController < ApplicationController
+    before_action :authenticate_user!, except: :index
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#index'
+
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root "splash#index", as: :unauthenticated_root
+    end
+  end
+  
   resources :users
 end
