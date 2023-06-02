17debf5890ec981d104d97c1ba642a9f437aba75
MAleemH <aleemhashmi4321@gmail.com>
2023-06-02 10:18:07 +0500

    Added Groups controller with views

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,30 @@
+class GroupsController < ApplicationController
+    def index
+        @groups = Group.all
+        @user = current_user
+    end
+    
+    def show
+        @group = Group.find(params[:id])
+    end
+    
+    def new
+        @group = Group.new
+    end
+    
+    def create
+        @group = Group.new(group_params)
+        @group.author_id = current_user.id
+        if @group.save
+            redirect_to authenticated_root_path, notice: 'Group was successfully created.'
+        else
+            render :new
+        end
+    end
+
+    private
+    
+    def group_params
+        params.require(:group).permit(:name, :icon)
+    end   
+end
