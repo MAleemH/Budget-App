92523e0e68c9b495cfa1c37bc8fa03208c418349
MAleemH <aleemhashmi4321@gmail.com>
2023-06-02 19:22:48 +0500

    Fixed rubocop errors

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,26 @@
 class GroupsController < ApplicationController
-    def index
-        @groups = Group.all
-        @user = current_user
-    end
-    
-    def new
-        @group = Group.new
-    end
-    
-    def create
-        @group = Group.new(group_params)
-        @group.author_id = current_user.id
-        if @group.save
-            redirect_to authenticated_root_path, notice: 'Group was successfully created.'
-        else
-            render :new
-        end
-    end
+  def index
+    @groups = Group.all
+    @user = current_user
+  end
 
-    private
-    
-    def group_params
-        params.require(:group).permit(:name, :icon)
-    end   
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.author_id = current_user.id
+    if @group.save
+      redirect_to authenticated_root_path, notice: 'Group was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,5 +1,4 @@
 class SplashController < ApplicationController
-    before_action :authenticate_user!, except: :index
-    def index
-    end
+  before_action :authenticate_user!, except: :index
+  def index; end
 end
